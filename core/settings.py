c53b73890d45c360c1f37e7a1ede56b9904b8e3e
ArrendifyDev <jsepulvedaarrendify@gmail.com>
2024-04-12 15:10:38 -0600

    cambio de db para prod

--- core/settings.py
+++ core/settings.py
@@ -197,16 +197,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "postgres",
-#         'USER': 'arrendify',
-#         'PASSWORD': 'Arrendy.123',
-#         'HOST': 'arrendifyapp.cnw0xwrwd9ir.us-east-2.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "postgres",
+        'USER': 'arrendify',
+        'PASSWORD': 'Arrendy.123',
+        'HOST': 'arrendifyapp.cnw0xwrwd9ir.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -219,12 +219,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
