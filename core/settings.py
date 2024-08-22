a163842a076f6600077b9f956a15313e3670781e
Arrendify <jsepulvedaarrendify@gmail.com>
2024-08-22 14:47:11 -0600

    correcion de obligado en pagare semillero

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = False
 
 LOGGING = {
     'version': 1,
